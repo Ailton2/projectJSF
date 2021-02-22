4acb5f3b5af2d3a503220836475a01ced052cc75
Ailton2 <tym.ailton@hotmail.com>
2021-02-21 22:29:02 -0300

    deletado pacote util com class

--- src/br/com/projetcjsf/util/VariavelConexaoUtil.java
+++
@@ -1,6 +0,0 @@
-package br.com.projetcjsf.util;
-//nome do caminha JNDI datasource tomcat
-public class VariavelConexaoUtil {
-
-	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
-}
