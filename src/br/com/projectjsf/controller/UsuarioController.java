1397138b0d803b0da076c6fc0e930a163ed7c134
Ailton2 <tym.ailton@hotmail.com>
2021-02-22 20:14:46 -0300

    inject UsuarioService

--- src/br/com/projectjsf/controller/UsuarioController.java
+++ src/br/com/projectjsf/controller/UsuarioController.java
@@ -1,13 +1,19 @@
 package br.com.projectjsf.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import br.com.projectjsf.model.Usuario;
+import br.com.projectjsf.service.UsuarioService;
 
 @Controller(value = "usuarioController")
 @Scope(value = "request")
 public class UsuarioController {
+	
+	
+	@Autowired
+	private UsuarioService usuarioService;
 	
 	
 	private Usuario usuario = new Usuario();
